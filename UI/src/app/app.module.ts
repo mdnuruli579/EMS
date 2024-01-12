64ac7f585224aa1e1b3417eb88199a71caec4fe2
Nurul Islam <nurul.islam@paynways.com>
2024-01-13 02:33:39 +0530

    added employee componenet

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -5,19 +5,26 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './components/footer/footer.component';
-
+import { EmployeeComponent } from './components/employee/employee.component';
+import { MatButtonModule } from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent,
+        EmployeeComponent,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatTableModule,
+        MatIconModule,
+    ]
 })
 export class AppModule { }
